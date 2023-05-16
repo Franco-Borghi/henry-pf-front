b1eea1ad21d5962825c5e9e9a7678aac0c50fbcd
Ramiro Roballos <ramiroroballos@gmail.com>
2023-05-15 20:54:19 -0400

    arme estructura de redux: store, reducer base y la estructura de algunas acciones

---
+++ src/redux/actions.js
@@ -0,0 +1,13 @@
+export const GET_ALL_MOTOS = "GET_ALL_MOTOS"
+export const GET_MOTOS_BY_NAME = "GET_MOTOS_BY_NAME"
+export const GET_MOTO_BY_ID = "GET_MOTO_BY_ID"
+
+
+export const getAllMotos = () => {
+}
+
+export const getMotosByName = (name) => {
+}
+
+export const getMotoById = (id) => {
+}
