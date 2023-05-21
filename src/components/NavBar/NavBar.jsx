352843108e9fc49cc507a48368978520c82938e0
Ivan <ivandaicich@gmail.com>
2023-05-21 00:47:36 -0300

    Se agrego el componente form

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import SearchBar from "../SearchBar/SearchBar";
 import styles from "./NavBar.module.scss";
 import logo from '../Footer/dinamo.png'
+import { Link } from "react-router-dom";
 
 export default function NavBar(props) {
   return (
@@ -10,7 +11,19 @@ export default function NavBar(props) {
 
         <SearchBar setSearchQuery={props.setSearchQuery}></SearchBar>
 
+
         <div className={styles.ctnIcons}>
+        <Link to="/create">
+          <div className={styles['icon-container']} >
+            <button className={styles.btnIcon}>
+              <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="create-outline"></ion-icon>
+            </button>
+            <p className={styles.txtBtnIcons}>Add Product</p>
+            </div>
+            </Link>
+          
+
+        {/* <div className={styles.ctnIcons}>
           <div className={styles['icon-container']} >
             <button className={styles.btnIcon}>
               <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="person-outline"></ion-icon>
@@ -30,7 +43,7 @@ export default function NavBar(props) {
               <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="person-outline"></ion-icon>
             </button>
             <p className={styles.txtBtnIcons}>Admin</p>
-          </div>
+          </div> */}
         </div>
       </div>
     </nav>
