a36783745f9f294ded0e22a8cf91d9913c78af40
Franco Borghi <francox93@hotmail.com>
2023-06-04 04:20:19 -0300

    users table done. Pending: filters, pagination

--- src/components/AdminDashboard/Users/Users.jsx
+++ src/components/AdminDashboard/Users/Users.jsx
@@ -1,39 +1,60 @@
 import React from "react";
-import styles from '../ItemsTable/ItemsTable.module.css'
+import { UserRow } from "./UserRow";
+import styles from './Users.module.scss';
 
 export default function Users(){
 
-    const [users, setUsers] = React.useState(null)
+    const [users, setUsers] = React.useState(null);
+
+    function convertCamelCase(str) {
+        // Add a space before each uppercase letter
+        var result = str.replace(/([A-Z])/g, ' $1');
+        
+        // Capitalize the first letter
+        result = result.charAt(0).toUpperCase() + result.slice(1);
+        
+        return result;
+      }
+
+    const getUsers = () => {
+        fetch(`${process.env.REACT_APP_HOST_NAME}/users`)
+            .then(response => response.json())
+            .then(data => setUsers(data))
+            .catch(err => console.error(err.message));
+    }
 
     React.useEffect(() => {
-        fetch(`${process.env.REACT_APP_HOST_NAME}/users`)
-        .then(response => response.json())
-        .then(data => setUsers(data))
-        .catch(err => console.error(err.message));
+        getUsers();
     }, [])
+
     return (
-        <div>USERS
+        <div className={styles.container}>
+            <h2>Users Table</h2>
             <table className={styles.table}>
-                <thead>
-                <tr>
-                    {
-                        users && users.length &&
-                        Object.keys(users[0]).map(el => (
-                            <th>{el}</th>
-                        ))
-                    }
-                </tr>
+                <thead className={styles.thead}>
+                    <tr className={styles.tr}>
+                        {
+                            users && users.length &&
+                            <>
+                                {
+                                    Object.keys(users[0]).map(el => {
+                                        if (el === 'orders') {
+                                            return <th className={styles.th}>Orders Number</th>
+                                        }
+                                        return <th className={styles.th}>{convertCamelCase(el)}</th>
+                                    })
+                                }
+                                <th className={styles.th}>Action</th>
+                            </>
+                        }
+                    </tr>
                 </thead>
-                <tbody className={styles.font}>
+                <tbody className={styles.tbody}>
                     {
                         users && users.length &&
                         users.map((el, i) => {
-                            console.log(el);
                             return (
-                                <tr>
-                                    <td>{el.id}</td>
-                                    <td>{el.firstName}</td>
-                                </tr>
+                                <UserRow key={i} user={el} getUsers={getUsers}/>
                             )
                         })
                     }
