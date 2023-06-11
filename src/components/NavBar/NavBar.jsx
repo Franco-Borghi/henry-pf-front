fd1f0091fb0e68e5acc1ac41fc2e3b4fe02e8dfe
Franco Borghi <francox93@hotmail.com>
2023-06-11 20:51:42 -0300

    Home section responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -22,6 +22,7 @@ export default function NavBar(props) {
   const reduxUser = useSelector(state => state.user);
   const { isAuthenticated, user } = useAuth0();
   const [cartItems, setCartItems] = React.useState(0);
+  const [dropdownActive, setDropdownActive] = React.useState(false);
 
   function onClickLogo (){
     navigate('/')
@@ -48,96 +49,169 @@ export default function NavBar(props) {
   }, [isAuthenticated])
 
   return (
-    <nav className={styles['nav-bar-container']}>
-      <div className={styles.ctnNavBar}>
-        <img onClick={onClickLogo} className={styles.logo} src={logo} alt="logo" />
+    <>
+      <nav className={styles['nav-bar-container']}>
+        <div className={styles.ctnNavBar}>
+          <img onClick={onClickLogo} className={styles.logo} src={logo} alt="logo" />
 
-        <SearchBar setSearchQuery={props.setSearchQuery} searchInput={searchInput}></SearchBar>
+          <SearchBar setSearchQuery={props.setSearchQuery} searchInput={searchInput}></SearchBar>
 
-        <div className={styles.ctnIcons}>
           <div className={styles.ctnIcons}>
+            <div className={styles.ctnIcons}>
 
-            <Link to={"/"}>
-              <div className={styles['icon-container']} >
-                <button className={styles.btnIcon}>
-                  <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="home"></ion-icon>
-                </button>
-                <p className={styles.txtBtnIcons}>Home</p>
+              <Link to={"/"}>
+                <div className={styles['icon-container']} >
+                  <button className={styles.btnIcon}>
+                    <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="home"></ion-icon>
+                  </button>
+                  <p className={styles.txtBtnIcons}>Home</p>
 
-              </div> 
-            </Link>
+                </div> 
+              </Link>
 
-            {
-              isAuthenticated  && reduxUser && reduxUser.role === 'client' && 
-                <Link to="/favourites">
-                  <div className={styles['icon-container']} >
-                    <button className={styles.btnIcon}>
-                      <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="heart-circle-sharp"></ion-icon>
-                    </button>
-                    <p className={styles.txtBtnIcons}>Favourites</p>
-                  </div>
-                </Link>
-            }
-
-            {
-              isAuthenticated  && reduxUser && reduxUser.role === 'client'
-              &&  <Link to="/shopping-cart">
-                      <div className={styles['icon-container']} >
-                        {
-                          shoppingCart.length
-                          ? <div className={styles.itemsNumber}>{cartItems}</div>
-                          : null
-                        }
-                        <button className={styles.btnIcon}>
-                          <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="cart-outline"></ion-icon>
-                        </button>
-                        <p className={styles.txtBtnIcons}>Cart</p>
-                      </div>
-                  </Link>
-            }
-
-            {
-              isAuthenticated  && reduxUser && reduxUser.role === 'admin'
-              &&  <Link to={"/admin"}>
+              {
+                isAuthenticated  && reduxUser && reduxUser.role === 'client' && 
+                  <Link to="/favourites">
                     <div className={styles['icon-container']} >
                       <button className={styles.btnIcon}>
-                        <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="person-outline"></ion-icon>
+                        <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="heart-circle-sharp"></ion-icon>
                       </button>
-                      <p className={styles.txtBtnIcons}>Admin</p>
-
-                    </div> 
+                      <p className={styles.txtBtnIcons}>Favourites</p>
+                    </div>
                   </Link>
-            }
-
-            <div className={styles['icon-container']} >
-              {
-                isAuthenticated && reduxUser  
-                ? <>
-                    <Link to={`/profile`}>
-                      {/* <div style={{height: '40px', width: '40px', borderRadius: '50%', cursor: 'pointer',backgroundColor: '#c7c7c7', backgroundImage: user && `url('${user.picture}')`, backgroundSize: 'cover', backgroundPosition: 'center'}}></div> */}
-                      <img style={{height: '40px', width: '40px', borderRadius: '50%', cursor: 'pointer', backgroundColor: '#c7c7c7'}} src={user && user.picture} />
-                      <p className={styles.txtBtnIcons}>Profile</p>
-                    </Link>
-                  </>
-                : <div style={{height: '40px', width: '40px'}} />
               }
+
+              {
+                isAuthenticated  && reduxUser && reduxUser.role === 'client'
+                &&  <Link to="/shopping-cart">
+                        <div className={styles['icon-container']} >
+                          {
+                            shoppingCart.length
+                            ? <div className={styles.itemsNumber}>{cartItems}</div>
+                            : null
+                          }
+                          <button className={styles.btnIcon}>
+                            <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="cart-outline"></ion-icon>
+                          </button>
+                          <p className={styles.txtBtnIcons}>Cart</p>
+                        </div>
+                    </Link>
+              }
+
+              {
+                isAuthenticated  && reduxUser && reduxUser.role === 'admin'
+                &&  <Link to={"/admin"}>
+                      <div className={styles['icon-container']} >
+                        <button className={styles.btnIcon}>
+                          <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="person-outline"></ion-icon>
+                        </button>
+                        <p className={styles.txtBtnIcons}>Admin</p>
+
+                      </div> 
+                    </Link>
+              }
+
+              <div className={styles['icon-container']} >
+                {
+                  isAuthenticated && reduxUser  
+                  ? <>
+                      <Link to={`/profile`}>
+                        {/* <div style={{height: '40px', width: '40px', borderRadius: '50%', cursor: 'pointer',backgroundColor: '#c7c7c7', backgroundImage: user && `url('${user.picture}')`, backgroundSize: 'cover', backgroundPosition: 'center'}}></div> */}
+                        <img style={{height: '40px', width: '40px', borderRadius: '50%', cursor: 'pointer', backgroundColor: '#c7c7c7'}} src={user && user.picture} />
+                        <p className={styles.txtBtnIcons}>Profile</p>
+                      </Link>
+                    </>
+                  : <div style={{height: '40px', width: '40px'}} />
+                }
+              </div>
+
+              <div className={styles['icon-container']} >
+                <button className={styles.btnIcon} style={{ width: 'fit-content', paddingLeft: '10px', paddingRight: '10px', color: '#fff'}}>
+                  {
+                    isAuthenticated 
+                    ? <LogoutBtn />
+                    : <LoginBtn />
+                  }
+                </button>
+              </div> 
+                  
             </div>
 
-            <div className={styles['icon-container']} >
-              <button className={styles.btnIcon} style={{ width: 'fit-content', paddingLeft: '10px', paddingRight: '10px', color: '#fff'}}>
+          </div>
+        </div>
+      </nav>
+
+      <nav className={styles['nav-bar-container-mobile']}>
+        <div className={styles['mobile-container']}>
+          <img onClick={onClickLogo} className={styles.logo} src={logo} alt="logo" />
+          <div className={styles['dropdown-container']}>
+            {
+              isAuthenticated && reduxUser  
+              ? <>
+                  <Link className={styles["logo-link"]} to={`/profile`}>
+                    {/* <div style={{height: '40px', width: '40px', borderRadius: '50%', cursor: 'pointer',backgroundColor: '#c7c7c7', backgroundImage: user && `url('${user.picture}')`, backgroundSize: 'cover', backgroundPosition: 'center'}}></div> */}
+                    <img style={{height: '100%', aspectRatio: '1 / 1', borderRadius: '50%', cursor: 'pointer', backgroundColor: '#c7c7c7'}} src={user && user.picture} />
+                  </Link>
+                </>
+              : null
+            }
+
+            <ion-icon onClick={() => setDropdownActive(!dropdownActive)} style={{ color: '#fff', fontSize: '25px', cursor: 'pointer'}} name="menu"></ion-icon>    
+          </div>
+        </div>
+        {
+          dropdownActive
+          ? <div className={styles['dropdown-content']}>
+              <div className={styles['searchbar-div']}>
+                <SearchBar setSearchQuery={props.setSearchQuery} searchInput={searchInput} setDropdownActive={setDropdownActive}></SearchBar>
+              </div>
+              <div className={styles['items']}>
+                <div onClick={() => {navigate('/'); setDropdownActive(false)}} style={{ display: 'flex', alignItems: 'center', gap: '10px'}}>                    
+                  <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="home"></ion-icon>
+                  Home
+                </div>
+                {
+                  isAuthenticated
+                  ? <>
+                      {
+                        isAuthenticated  && reduxUser && reduxUser.role !== 'admin'
+                        ? <>
+                            <div onClick={() => {navigate('/favourites'); setDropdownActive(false)}} style={{ display: 'flex', alignItems: 'center', gap: '10px'}}>
+                              <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="heart-circle-sharp"></ion-icon>
+                              Favourites
+                            </div>
+                            <div onClick={() => {navigate('/shopping-cart'); setDropdownActive(false)}} style={{ display: 'flex', alignItems: 'center', gap: '10px'}}>
+                              <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="cart-outline"></ion-icon>
+                              Cart
+                            </div>
+                          </>
+                        : <div onClick={() => {navigate('/admin'); setDropdownActive(false)}} style={{ display: 'flex', alignItems: 'center', gap: '10px'}}>
+                            <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="person-outline"></ion-icon>
+                            Admin
+                          </div>
+                      }
+                      <div onClick={() => {navigate('/profile'); setDropdownActive(false)}} style={{ display: 'flex', alignItems: 'center', gap: '10px'}}>
+                        <ion-icon style={{ color: "#fff"}} className='svg' size="small" name="person-circle"></ion-icon>
+                        Profile
+                      </div>
+                    </>
+                  : null
+                }
                 {
                   isAuthenticated 
-                  ? <LogoutBtn />
-                  : <LoginBtn />
+                  ? <div className={styles['logout']}>
+                      <LogoutBtn />
+                    </div>
+                  : <div className={styles['login']}>
+                      <LoginBtn />
+                    </div>
                 }
-              </button>
-            </div> 
-                
-          </div>
-
-        </div>
-      </div>
-    </nav>
+              </div>
+            </div>
+          : null
+        }
+      </nav>
+    </>
   );
 }
 
